6dbcc3e8f73a16a37e044131f6f19897f1556de2
Tom <TomEisenbrandt@gmail.com>
2018-11-01 12:26:48 -0500

    -

---
+++ header.php
@@ -0,0 +1,24 @@
+
+<html>
+
+<!--
+Header
+-->
+
+<head>
+<title>GEM System</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<hr>
+
+<p STYLE="TEXT-ALIGN:CENTER">
+<a href="index.html">Home</a> -
+<a href="createAgent.html">New Agent</a> -
+<a href="createVendor.html">New Vendor</a> -
+<a href="createEvent.html">New Event</a> -
+<a href="approveEvent.html"">Approve Event</a> -
+<a href="generateEventStatusReport.html"">Generate Event Status Report</a> -
+<a href="produceEventStatusReport.html">Produce Event Status Report</a>
+<hr>
+</p>
